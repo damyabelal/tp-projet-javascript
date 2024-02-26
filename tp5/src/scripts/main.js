3164f0ef68527e31c1bfd361c3586adf0654131f
Jean-Christophe <jean-christophe.routier@univ-lille.fr>
2024-02-26 11:27:59 +0100

    Renommage fichier : ajout ".component"

--- tp5/src/scripts/main.js
+++ tp5/src/scripts/main.js
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 // import ReactJs components
-import App from '../components/app.jsx';
+import App from '../components/app.component.jsx';
 
 /*
 * create React root element and insert it into document
