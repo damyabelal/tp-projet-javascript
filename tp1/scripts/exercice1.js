b44eb41f1cbef9a9249a30f2a5780581188114f6
Jean-Christophe <jean-christophe.routier@univ-lille.fr>
2025-01-15 11:40:58 +0100

    update fichiers projet1 24-25

--- tp1/scripts/exercice1.js
+++ tp1/scripts/exercice1.js
@@ -1,38 +1,40 @@
 'use strict';
 
-// une liste pour des tests
+// des listes pour des tests
 const numbers = [2, 3, 5, 4, 10, 6];
+const persons = [ {name : 'timoleon', age : 12 }, {name : 'bilbo', age : 111 }, {name : 'frodo', age : 33 }, {name : 'sam', age : 26 }];
 
-/********** EXERCICE 0 ***********************/
-console.log(` *** EXERCICE 0 *** `);
+/********** EXERCICE 1 ***********************/
+console.log(` *** EXERCICE 1 *** `);
+
+// exemple de manière de répondre aux questions d'un exercice
+
+// Q1
 /* computes the double of its parameter
  * @param x (number) a number
  * @return (number) the double of *x*
 */
 const example = x => x * 2;
-// tests
+
+//Q2
+// tests d'exécution de la fonction example
 console.log(`example(10) : ${example(10)}`);
 console.log(`example(21) : ${example(21)}`);
 
-
-
+// Q3
 /* filter and keep the elements of *list* smaller than *max*
  * @param list (Array) list of elements
  * @param max (Any) upper bound filter value
  * @return (Array) list of elements of *list* smaller than *max*
 */
 const example2 = (list, max) => list.filter( elt => elt < max );
-// tests
+
+// Q4
+// tests d'exécution de la fonction example2
 console.log(`example2(numbers, 5) : ${example2(numbers, 5)}`);
 
 /*********************************************/
 
-/********** EXERCICE 1 ***********************/
-console.log(` *** EXERCICE 1 *** `);
-const persons = [ {name : 'timoleon', age : 12 }, {name : 'bilbo', age : 111 }, {name : 'frodo', age : 33 }];
-
-
-/*********************************************/
 
 
 /********** EXERCICE 2 ***********************/
@@ -64,6 +66,20 @@ console.log(` *** EXERCICE 5 *** `);
 /********** EXERCICE 6 ***********************/
 console.log(` *** EXERCICE 6 *** `);
 
+
+/*********************************************/
+
+
+/********** EXERCICE 7 ***********************/
+console.log(` *** EXERCICE 7 *** `);
+
+
+/*********************************************/
+
+
+/********** EXERCICE 8 ***********************/
+console.log(` *** EXERCICE 8 *** `);
+
 const lesInvites = ['Tim Oleon', 'Timo Leon', 'Bilbo', 'Frodo', 'Sam', 'Merry', 'Pippin']
 const lesReponses = [
                   {nom : 'Sam', present : 'oui'},
@@ -75,13 +91,15 @@ const lesReponses = [
 
 /*********************************************/
 
-/********** EXERCICE 7 ***********************/
-console.log(` *** EXERCICE 7 *** `);
+/********** EXERCICE 9 ***********************/
+console.log(` *** EXERCICE 9 *** `);
+
+
 
 /*********************************************/
 
-/********** EXERCICE 8 ***********************/
-console.log(` *** EXERCICE 8 *** `);
+/********** EXERCICE 10 ***********************/
+console.log(` *** EXERCICE 10 *** `);
 
 
 
