2453d6cbcf667efb6c48d388651af29925357492
Belal Damya <damya.belal.etu@a14p23.fil.univ-lille.fr>
2025-03-12 14:34:56 +0100

    résolution Q4-EXO4

--- tp2/src/scripts/AnimationWithObstacle.js
+++ tp2/src/scripts/AnimationWithObstacle.js
@@ -11,14 +11,20 @@ export default class AnimationWithObstacle extends Animation {
   }
 
   animate() {
+    const ctxt = this.canvas.getContext("2d");
+    ctxt.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.obstacle.handleMoveKeys(this.keyManager);
+    this.obstacle.move(this.canvas);
+    this.obstacle.draw(ctxt);
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.balls.forEach(ball => ball.move(this.canvas));
     this.balls = this.balls.filter(ball => !ball.collisionWith(this.obstacle));
     this.obstacle.draw(this.context);
     this.balls.forEach(ball => ball.draw(this.context));
     this.req = window.requestAnimationFrame(this.animate.bind(this));
+
   }
-  
+
   keyDownActionHandler(event) {
     switch (event.key) {
         case "ArrowLeft":
