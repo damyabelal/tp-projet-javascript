827d5cba53a44f3efd31b0c3587af2b7c16a33a0
Belal Damya <damya.belal.etu@a10p14.fil.univ-lille.fr>
2025-03-12 12:24:59 +0100

    finalisation de l'EXO2 - Balles Multiples

--- tp2/src/scripts/main.js
+++ tp2/src/scripts/main.js
@@ -1,27 +1,23 @@
-
-import Ball from './ball';           // peut être commenté après la Ex1 Q2 faite
+import Ball from './ball';           // Peut être commenté après Ex1 Q2
 import Animation from './animation';
 
 import './assets/style/style-balles.css';
-
 
 /* setup */
 const init = () => {
   const canvas = document.getElementById("terrain");
 
-  // commenter les 2 lignes suivantes après la Ex1 Q2
-  //const ball = new Ball(50,50);
-  //document.getElementById("stopStartBall").addEventListener("click", () => ball.draw(canvas.getContext('2d')));
+  // commenter les 2 lignes suivantes après Ex1 Q2
+  // const ball = new Ball(50,50);
+  // document.getElementById("stopStartBall").addEventListener("click", () => ball.draw(canvas.getContext('2d')));
 
-   //décommenter les deux lignes suivantes à partir la question Ex1 Q4
   const animation = new Animation(canvas);
-  document.getElementById("stopStartBall").addEventListener("click", () => animation.startAndStop()  );
-  
+  document.getElementById("stopStartBall").addEventListener("click", () => animation.startAndStop());
+
+  document.getElementById("addBall").addEventListener("click", () => animation.addBall());
 }
 
-window.addEventListener("DOMContentLoaded",init);
-
+window.addEventListener("DOMContentLoaded", init);
 
 //
 console.log('le bundle a été généré');
-
