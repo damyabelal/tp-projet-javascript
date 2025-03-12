ea78b4e15c0adb9d1e1d3afa20814b0d82c82644
Belal Damya <damya.belal.etu@a10p14.fil.univ-lille.fr>
2025-03-12 12:28:19 +0100

    réponse a la Q1-EXO3

---
+++ tp2/src/scripts/obstacle.js
@@ -0,0 +1,16 @@
+import Ball from './ball';
+
+export default class Obstacle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw(context) {
+        context.fillStyle = 'black';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
