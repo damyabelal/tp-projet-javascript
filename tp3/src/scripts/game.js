e1a3bfc42114eeb65676757ffecad2d22e59ebeb
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-03-15 11:46:14 +0100

    amelioration de la classe game

---
+++ tp3/src/scripts/game.js
@@ -0,0 +1,28 @@
+
+import StarShip from './StarShip';
+
+export default class Game {
+  constructor(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.context = this.canvas.getContext('2d');
+    this.starship = new StarShip(40, this.canvas.height / 2);  
+    this.req = null;
+  }
+
+  animate() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); 
+
+    this.starship.move(this); 
+    this.starship.draw(this.context); 
+
+    this.req = window.requestAnimationFrame(this.animate.bind(this)); 
+  }
+
+  start() {
+    this.animate(); 
+  }
+
+  stop() {
+    window.cancelAnimationFrame(this.req); 
+  }
+}
