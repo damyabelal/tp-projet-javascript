58098739d7106732686dbb3f422cb38553302e87
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-03-19 12:54:32 +0100

    ajout de la methode collisionWith() qui renvoie true si le shoot(this) rentre en collision avec un mobile false sinon

--- tp3/src/scripts/shoot.js
+++ tp3/src/scripts/shoot.js
@@ -5,4 +5,15 @@ export default class Shoot extends Mobile {
   constructor(x, y) {
     super(x, y, shootImg, 8, 0);
   }
+
+  collisionWith(mobile) {
+    const p1x = Math.max(this.x, mobile.x);
+    const p1y = Math.max(this.y, mobile.y);
+    const p2x = Math.min(this.x + this.image.width, mobile.x + mobile.image.width);
+    const p2y = Math.min(this.y + this.image.height, mobile.y + mobile.image.height);
+    return p1x < p2x && p1y < p2y;
 }
+
+}
+
+
