a4b7fbe7737aae6363dcd98a9fd196c6f55c55ee
Belal Damya <damya.belal.etu@a13p23.fil.univ-lille.fr>
2025-03-24 13:57:21 +0100

    ajout de la documentation dans saucer.js

--- tp3/src/scripts/saucer.js
+++ tp3/src/scripts/saucer.js
@@ -1,13 +1,26 @@
 import Mobile from './mobile';
 import saucerImg from './assets/images/flyingSaucer-petit.png';
 
+/**
+ * class representing a saucer
+ * @extends Mobile
+ */
 export default class Saucer extends Mobile {
 
+  /**
+   * create a saucer
+   * @param {number} x - the x-coordinate of the saucer
+   * @param {number} y - the y-coordinate of the saucer
+   */
   constructor(x, y) {
     super(x, y, saucerImg, -3, 0);
     this.falling = false;
   }
 
+  /**
+   * move the saucer
+   * @param {Object} game - the game instance
+   */
   move(game) {
     this.x += this.deltaX;
     this.y += this.deltaY;
@@ -21,10 +34,17 @@ export default class Saucer extends Mobile {
     }
   }
 
+  /**
+   * check if the saucer is falling
+   * @returns {boolean} true if the saucer is falling, false otherwise
+   */
   isFalling(){
     return this.falling;
   }
   
+  /**
+   * make the saucer fall
+   */
   fall() {
     this.falling = true;
     this.deltaX = 0;
