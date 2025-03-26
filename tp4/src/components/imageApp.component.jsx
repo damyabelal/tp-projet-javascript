746fa13fb91f494f18ab97cd8564dbfbc28b68a4
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-03-26 09:53:47 +0100

    integration des composants ImageWall et ImageDetails au composant ImageApp

--- tp4/src/components/imageApp.component.jsx
+++ tp4/src/components/imageApp.component.jsx
@@ -2,10 +2,14 @@ import '../assets/style/murImages.css';
 
 import dataImages from "../data/dataImages.js"
 
+import ImageWall from "./imageWall.component.jsx";
+import ImageDetails from "./imageDetails.component.jsx";
+
 const ImageApp = () => {
 
   return (
-    <div>Image App</div>
+    <div> <ImageWall /> <ImageDetails /></div>
   );
 }
 export default ImageApp;
+ 
