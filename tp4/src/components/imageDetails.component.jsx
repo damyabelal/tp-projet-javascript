7ab9cb2004ba9d0493ac36f9b9b875b0d918f64e
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-03-26 09:45:23 +0100

    codage du composant ImageDetails

---
+++ tp4/src/components/imageDetails.component.jsx
@@ -0,0 +1,12 @@
+import '../assets/style/murImages.css';
+
+import dataImages from "../data/dataImages.js"
+
+const ImageDetails = () => {
+    return (
+    <div id="details">details</div>
+    );
+  }
+  
+export default ImageDetails;
+  
