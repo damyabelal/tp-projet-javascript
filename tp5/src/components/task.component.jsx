1384d9f15e5ce6a814fec2dd679af2fd93179486
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-07 16:36:14 +0200

    ajout de la priorité dans le composent task

--- tp5/src/components/task.component.jsx
+++ tp5/src/components/task.component.jsx
@@ -6,6 +6,27 @@ import '../assets/style/task.css';
 const Task = ({ task, setTasksToDo, setTasksDone }) => {
   const { id, description, duration, priority, done } = task;
 
-}
+  const priorityChange = newPriority => {
+    task.priority = newPriority;
+    setTasksToDo(prevTasks => [...prevTasks]);
+  };
+
+  const markIsDone = () => {
+    task.done = true;
+    setTasksToDo(prevTasks => prevTasks.filter(t => t.id !== id));
+    setTasksDone(prevTasks => [...prevTasks, task]);
+  };
+
+  return (
+    <div className={`task ${done ? 'done' : ''}`}>
+      <div className="info">
+        <p>{description}</p>
+        <p> durée: {duration}</p>
+        </div>
+      <PriorityScale priority={priority} setPriority={priorityChange} />
+      {!done && <DoneButton onClick={markIsDone} />}
+    </div>
+  );
+};
 
 export default Task;
