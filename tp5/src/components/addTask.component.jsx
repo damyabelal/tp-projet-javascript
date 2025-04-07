b4caf1d7e17028c0de48a74ee1833119357423cf
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-07 17:02:46 +0200

    ajout du composant AddTask avec les états pour la description et la durée de la tâche

---
+++ tp5/src/components/addTask.component.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../assets/style/addtask.css';
+
+const AddTask = ({ setTasksToDo }) => {
+  const [taskDescription, setTaskDescription] = useState('');
+  const [taskDuration, setTaskDuration] = useState(0);
+
+  
+};
+
+export default AddTask;
