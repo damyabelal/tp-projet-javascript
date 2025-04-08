fa8416798776c8b2944913f5fd13d3dc66b5ab99
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 11:27:41 +0200

    ajout de la fonction AddTask pour créer une nouvelle tâche avec description, durée, priorité par défaut et statut non terminé

--- tp5/src/components/addTask.component.jsx
+++ tp5/src/components/addTask.component.jsx
@@ -5,7 +5,20 @@ const AddTask = ({ setTasksToDo }) => {
   const [taskDescription, setTaskDescription] = useState('');
   const [taskDuration, setTaskDuration] = useState(0);
 
-  
+  const addTask = () => {
+    const newTask = {
+      id: `T${Date.now()}`,
+      description: taskDescription,
+      duration: taskDuration,
+      priority: 1,
+      done: false,
+    };
+    setTasksToDo(prevTasks => [...prevTasks, newTask]);
+    setTaskDescription('');
+    setTaskDuration(0);
+  };
+
+
 };
 
 export default AddTask;
