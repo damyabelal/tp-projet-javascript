2b6ffd794663aaa1ad571a991e493432477a3856
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 16:18:01 +0200

    ajout de la documentation pour le composant task

--- tp5/src/components/task.component.jsx
+++ tp5/src/components/task.component.jsx
@@ -3,6 +3,16 @@ import PriorityScale from './priorityScale.component.jsx';
 import DoneButton from './doneButton.component.jsx';
 import '../assets/style/task.css';
 
+/**
+ * task is a component to show one task in the list
+ * it shows the description the duration and the priority scale
+ * the user can change the priority or click to mark the task as done
+ * when marked as done it moves the task from todo list to done list
+ * 
+ * @param {object} task - the task object with id description duration priority and done
+ * @param {function} setTasksToDo - the function to update the todo list
+ * @param {function} setTasksDone - the function to update the done list
+ */
 const Task = ({ task, setTasksToDo, setTasksDone }) => {
   const { id, description, duration, priority, done } = task;
 
