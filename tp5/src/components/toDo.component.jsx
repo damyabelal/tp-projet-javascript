8464dc35892dba37fd6c0075f00fb5f3caaa32e8
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 16:20:49 +0200

    ajout de la documentation pour le composant toDo

--- tp5/src/components/toDo.component.jsx
+++ tp5/src/components/toDo.component.jsx
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import Task from './task.component.jsx';
 import '../assets/style/tasklist.css';
 
-
+/**
+ * todo shows all tasks that are not done yet
+ * user can type text to filter the tasks by description
+ * tasks are sorted by priority from high to low
+ * it also shows how many tasks are left and their total duration
+ * each task is shown using the task component
+ * 
+ * @param {array} tasks - the list of tasks to do
+ * @param {function} setTasksToDo - function to update the todo list
+ * @param {function} setTasksDone - function to update the done list
+ */
 
 const Todo = ({ tasks, setTasksToDo, setTasksDone }) => {
   const [searchText, setSearchText] = useState(''); 
@@ -26,7 +36,5 @@ const Todo = ({ tasks, setTasksToDo, setTasksDone }) => {
       ))}
     </div>
 );
-
 }
-
 export default Todo;
