1882ad203bd55f9bef41cd1d329d1c48ca90c87f
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 13:12:41 +0200

    ajout de l'affichage des tâches terminées avec le composant DoneTask

---
+++ tp5/src/components/done.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DoneTask from './doneTask.component.jsx';
+
+
+const Done = ({ tasks }) => (
+  <div className="tasklist done">
+    <h3>Tâches terminées </h3>
+  </div>
+);
+
+export default Done;
