7752fea3ff26124131f0c8220ef0530e442c9f8c
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 12:33:35 +0200

    affichage du nombre total de tâches filtrées et de leur durée cumulée

--- tp5/src/components/toDo.component.jsx
+++ tp5/src/components/toDo.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Task from './task.component.jsx';
 import '../assets/style/tasklist.css';
+import Sum from './sum.component.jsx';
 
 
 const Todo = ({ tasks, setTasksToDo, setTasksDone }) => {
@@ -17,6 +18,18 @@ const Todo = ({ tasks, setTasksToDo, setTasksDone }) => {
     <div className="tasklist">
       <h3>Tâches en cours</h3>
       <div className="filter"><input type="text" placeholder="filtrer" value={searchText} onChange={search}/></div>
+      <div className="info">
+        Il y'a {filteredTasks.length} tâches en cours. Pour une durée de : {filteredTasks.reduce((acc, task) => acc + Number(task.duration), 0)} mn
+      </div>
+
+      {sortedTasks.map(task => (
+        <Task 
+          key={task.id} 
+          task={task} 
+          setTasksToDo={setTasksToDo} 
+          setTasksDone={setTasksDone} 
+        />
+      ))}
     </div>
 );
 
