480e4ad61085083069e343fbbe4d36ae57175b3a
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 16:08:33 +0200

    ajout de la documentation pour le composant doneTask

--- tp5/src/components/doneTask.component.jsx
+++ tp5/src/components/doneTask.component.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * donetask is a component that shows one task that is done
+ * it uses the task prop to show the description and the duration
+ * the task is shown with a special style for done tasks
+ * 
+ * @param {object} task - the task object with description and duration
+ * @returns the visual block of a done task
+ */
 
 const DoneTask = ({ task }) => (
   <div className="task done">
