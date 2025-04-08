72eac259bba99273c13d0e251327ffb68830bf88
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 13:15:02 +0200

    affichage des tâches terminées avec le composant DoneTask

--- tp5/src/components/done.component.jsx
+++ tp5/src/components/done.component.jsx
@@ -5,6 +5,9 @@ import DoneTask from './doneTask.component.jsx';
 const Done = ({ tasks }) => (
   <div className="tasklist done">
     <h3>Tâches terminées </h3>
+    {tasks.map(task => (
+      <DoneTask key={task.id} task={task} />
+    ))}
   </div>
 );
 
