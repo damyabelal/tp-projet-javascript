6dccefba39910cb68ddff5aa701d5e4493edcfb8
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 13:37:49 +0200

    modification du tableat tasks pour remettre la priorité à 1

--- tp5/src/data/tasksData.js
+++ tp5/src/data/tasksData.js
@@ -2,22 +2,26 @@ const tasks = [
   {
     id : 'T1',
     description : "manger",
-    duration : 20
+    duration : 20,
+    priority: 1
   },
   {
     id : 'T2',
     description : "finir le projet de JS",
-    duration : 90
+    duration : 90,
+    priority: 1
   },
   {
     id : 'T3',
     description : "faire une sieste",
-    duration : 60
+    duration : 60,
+    priority: 1
   },
   {
     id : 'T4',
     description : "prendre un café",
-    duration : 5
+    duration : 5,
+    priority: 1
   }
 ];
 
