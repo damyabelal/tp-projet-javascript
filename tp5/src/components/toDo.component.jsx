03ec06ca854320e42b5c48f5a157e90f019c93b0
damya.belal.etu <damya.belal.etu@univ-lille.fr>
2025-04-08 12:01:02 +0200

    ajout d’un champ de recherche pour filtrer les tâches en cours par description

---
+++ tp5/src/components/toDo.component.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Task from './task.component.jsx';
+import '../assets/style/tasklist.css';
+
+
+const Todo = ({ tasks, setTasksToDo, setTasksDone }) => {
+  const [searchText, setSearchText] = useState(''); 
+
+};
+
+export default Todo;
